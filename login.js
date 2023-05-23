f5b71cd3ef7b1647269ab137483203719f8c963e
MefeKartvela <132370629+MefeKartvela@users.noreply.github.com>
2023-05-23 14:53:33 +0400

    Create login.js

---
+++ login.js
@@ -0,0 +1,28 @@
+
+const forms = document.querySelector(".forms"),
+passwShowHide = document.querySelectorAll(".eye-icon"),
+links = document.querySelectorAll(".link");
+
+passwShowHide.forEach(eyeIcon => {
+eyeIcon.addEventListener("click", () => {
+  let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+  
+  pwFields.forEach(password => {
+      if(password.type === "password"){
+          password.type = "text";
+          eyeIcon.classList.replace("bx-hide", "bx-show");
+          return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+  })
+  
+})
+})      
+
+links.forEach(link => {
+link.addEventListener("click", e => {
+ e.preventDefault();
+ forms.classList.toggle("show-signup");
+})
+})
