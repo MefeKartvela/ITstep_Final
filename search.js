8b8ee62d359ca149f39d0ee6ba4115cd80bb89e8
MefeKartvela <132370629+MefeKartvela@users.noreply.github.com>
2023-05-26 22:46:39 +0400

    Create search.js

---
+++ search.js
@@ -0,0 +1,69 @@
+const product = [
+    {
+        id: 0,
+        image: 'img/img1.png',
+        title: 'Chocholate Cake',
+        price: 120,
+    },
+    {
+        id: 1,
+        image: 'img/cake.jpg',
+        title: 'Fruit Cake',
+        price: 60,
+    },
+    {
+        id: 2,
+        image: 'img/chs.jpg',
+        title: 'Cheese Cake',
+        price: 230,
+    },
+    {
+        id: 3,
+        image: 'img/muffin.jpg',
+        title: 'Muffin',
+        price: 100,
+    },
+    {
+        id: 4,
+        image: 'img/skakee.JPG',
+        title: 'Cake With Waffles',
+        price: 230,
+    },
+    {
+        id: 5,
+        image: 'img/sckae.jpg',
+        title: 'Dark Chocolate Cake',
+        price: 100,
+    },
+];
+
+const categories = [...new Set(product.map((item) => { return item }))]
+
+document.getElementById('searchBar').addEventListener('keyup', (e) => {
+    const searchData = e.target.value.toLowerCase();
+    const filteredData = categories.filter((item) => {
+        return (
+            item.title.toLowerCase().includes(searchData)
+        )
+    })
+    displayItem(filteredData)
+});
+
+const displayItem = (items) => {
+    document.getElementById('root').innerHTML = items.map((item) => {
+        var { image, title, price } = item;
+        return (
+            `<div class='box'>
+                <div class='img-box'>
+                    <img class='images' src=${image}></img>
+                </div> 
+                <div class='bottom'>
+                    <p>${title}</p>
+                    <h2>$ ${price}.00</h2>
+                <button>Add to cart</button>
+                </div>
+            </div>`
+        )
+    }).join('')
+};
+displayItem(categories);
