55d6cb65c582682b9b5113b2d226306359607c8a
MefeKartvela <132370629+MefeKartvela@users.noreply.github.com>
2023-05-30 16:57:15 +0400

    Update login.js

--- login.js
+++ login.js
@@ -26,20 +26,83 @@ link.addEventListener("click", e => {
 })
 })
 
-fetch('http://kketelauri-001-site1.gtempurl.com/api/user/adduser', {
-  method: 'POST',
-  headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({ 
-      "userName": "keti4",
-  "firstName": "keti4",
-  "lastName": "ketelauri4",
-  "email": "keti4@gmail.com",
-  "privateNumber": "12345678914",
-  "password": "123",
-  "active": true})
+const forms = document.querySelector(".forms"),
+passwShowHide = document.querySelectorAll(".eye-icon"),
+links = document.querySelectorAll(".link");
+
+passwShowHide.forEach(eyeIcon => {
+eyeIcon.addEventListener("click", () => {
+  let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+  
+  pwFields.forEach(password => {
+      if(password.type === "password"){
+          password.type = "text";
+          eyeIcon.classList.replace("bx-hide", "bx-show");
+          return;
+      }
+      password.type = "password";
+      eyeIcon.classList.replace("bx-show", "bx-hide");
+  })  
+  
 })
- .then(response => response.json())
- .then(response => console.log(JSON.stringify(response)))
+})      
+
+links.forEach(link => {
+link.addEventListener("click", e => {
+ e.preventDefault();
+ forms.classList.toggle("show-signup");
+})
+})
+
+function func2() {
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+  
+    fetch('https://kketelauri-001-site1.gtempurl.com/api/user/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "email": email,
+        "password": password
+      })
+    })
+    .then(response => response.json())
+    .then(response => {
+      console.log(JSON.stringify(response));
+      localStorage.setItem('loggedInUser', JSON.stringify(response));
+      window.location.href = 'index.html';
+      let login = true
+    })
+    .catch(response => console.log("error"));
+  }
+
+  function func1(event) {
+    event.preventDefault()
+    let user = document.getElementById("username").value
+    let firstname = document.getElementById("firstname").value
+    let lastname = document.getElementById("lastname").value
+    let email = document.getElementById("email").value
+    let privatenum = document.getElementById("privatenumber").value
+    let password = document.getElementById("password").value
+    
+    fetch('https://kketelauri-001-site1.gtempurl.com/api/user/adduser', {
+    method: 'POST',
+    headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+    },
+     body: JSON.stringify({
+     "userName": user,
+     "firstName": firstname,
+     "lastName": lastname,
+     "email": email,
+     "privateNumber": privatenum,
+     "password": password,
+     "active": true})
+    })
+    .then(response => response.json())
+    .then(response => console.log(JSON.stringify(response)))   
+}
